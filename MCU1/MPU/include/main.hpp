ec6ef92248aacf1052fb1b878b0c59c3f46ea7cb
Yubo Wang <yubo0809@outlook.com>
2024-05-08 23:57:06 -0400

    prep for lsm6ds3 inertial sensor

--- MCU1/MPU/include/main.hpp
+++ MCU1/MPU/include/main.hpp
@@ -4,9 +4,31 @@
 #define USE_FULL_LL_DRIVER
 
 #include "stm32h7xx_hal.h"
+#include "stm32h7xx_hal_i2c.h"
+#include "stm32h7xx_hal_gpio.h"
+#include "stm32h7xx_hal_rcc.h"
+#include "stm32h7xx_hal_rcc_ex.h"
+#include "stm32h7xx_hal_usart.h"
+#include "stm32h7xx_hal_uart.h"
+
 
 #define LED_PIN                                GPIO_PIN_1
 #define LED_GPIO_PORT                          GPIOE
 #define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+
+//****** Function Prototypes ******//
+
+// handlers for libraries
+extern "C" static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+extern "C" static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+
+// Others
+void initialize();
+
+
+
+//****** global variables ******//
 
 #endif // MAIN_H
--- MCU1/MPU/src/main.cpp
+++ MCU1/MPU/src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.hpp"
+#include "lsm6ds3tr-c_reg.h"
 
 void LED_Init();
 
@@ -23,6 +24,22 @@ void LED_Init()
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+}
+
+//****** handlers for libraries ******//
+
+extern "C" static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+  HAL_I2C_Mem_Write((I2C_HandleTypeDef*)handle, LSM6DS3TR_C_I2C_ADD_L, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
+}
+
+extern "C" static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+  HAL_I2C_Mem_Read((I2C_HandleTypeDef*)handle, LSM6DS3TR_C_I2C_ADD_L, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
 }
 
 extern "C" void SysTick_Handler(void)
